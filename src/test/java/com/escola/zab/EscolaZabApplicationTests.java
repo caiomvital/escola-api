57920803af9acedc73643f77aed7e7770ec273bc
caiomvital <caiomvital@gmail.com>
2024-08-28 19:19:47 -0300

    commit inicial

---
+++ src/test/java/com/escola/zab/EscolaZabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escola.zab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaZabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
